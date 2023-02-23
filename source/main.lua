72385e5c4a5ea6200e5774fda76573a8abcf52da
Scott King <jsking@andrew.cmu.edu>
2023-02-23 17:25:06 -0500

    Rearrange template

--- source/main.lua
+++ source/main.lua
@@ -2,7 +2,7 @@ import "dvd" -- DEMO
 local dvd = dvd(1, -1) -- DEMO
 
 local gfx <const> = playdate.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
+local font = gfx.font.new('Fonts/Mini Sans 2X') -- DEMO
 
 local function loadGame()
 	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
